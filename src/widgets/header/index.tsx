f2a169173ef88c967b20b8cfe6551d5c8200ff8d
Alisher <zumanovaliser814@gmail.com>
2026-01-08 01:05:30 +0600

    header

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@heroui/react";
 import { LangSwitcher } from "@/shared/ui/lang-switcher";
 import { Logo } from "@/shared/ui/logo";
 
@@ -7,8 +8,12 @@ export const Header = () => {
       <div className="max-w-400 m-auto p-5 flex justify-between items-center">
         <Logo />
 
-        <div>
+        <div className="flex items-center gap-4">
           <LangSwitcher />
+
+          <Button className="bg-blue-700 rounded-xl font-medium text-xl py-4 px-5 h-fit w-fit">
+            Кирүү
+          </Button>
         </div>
       </div>
     </header>
